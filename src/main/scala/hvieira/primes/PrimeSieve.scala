82e59ef0d6b8208f8332e08fd3c81dbd2f9256d6
hvieira <scythe999@gmail.com>
2017-05-21 19:37:40 +0100

    Greatly improved performance of prime computation by using Streams

--- src/main/scala/hvieira/primes/PrimeSieve.scala
+++ src/main/scala/hvieira/primes/PrimeSieve.scala
@@ -2,37 +2,18 @@ package hvieira.primes
 
 object PrimeSieve {
 
-  private val sieveRange = 300000
-
-  private def computePrimes(): List[Int] = {
-
-    def inner(primes: List[Int], currentNum: Int): List[Int] = {
-
-      if (currentNum > sieveRange)
-        primes
-      else {
-        // reduce search space
-        val possibleDivisors = primes.takeWhile(prime => Math.pow(prime, 2) <= currentNum)
-
-        // if is prime
-        if (possibleDivisors.forall(d => currentNum % d != 0))
-          inner(primes :+ currentNum, currentNum + 1)
-        else
-          inner(primes, currentNum + 1)
-      }
-
-    }
-
-    inner(List(2), 3)
-  }
-
-  val primes = computePrimes()
+  private val primes: Stream[Int] =
+  // seed a stream with 2 as an already computed prime and then start from 3 lazily
+    2 #:: Stream.from(3)
+      // get primes until their square is greater than the candidate number -> reduces search space
+      // for all those primes verify that the candidate does not have any of them as divisor
+      .filter(candidate => primes.takeWhile(prime => Math.pow(prime, 2) <= candidate).forall(prime => candidate % prime > 0))
 
   def getPrimes(numberOfPrimes: Int): List[Int] = {
     if (numberOfPrimes < 0)
       throw new IllegalArgumentException("Cannot generate primes until a negative number")
     else
-      primes.slice(0, numberOfPrimes)
+      primes.take(numberOfPrimes).toList
   }
 
 }
